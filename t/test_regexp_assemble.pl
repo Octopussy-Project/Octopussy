2285727a01078d8ddc9edc199560f76ec30962c5
sebthebert <sebthebert@6a9e80cc-ef52-47e4-9be5-917275ac7a98>
2010-02-11 17:59:34 +0000

    Need to work on 'services integrity' Test from 2 previous scripts

---
+++ t/test_regexp_assemble.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+use Regexp::Assemble;
+
+use Octopussy;
+use Octopussy::Message;
+use Octopussy::Service;
+
+
+my @services = ($ARGV[0]) || Octopussy::Service::List();
+my $nb_ok = 0;
+my %invalid;
+
+printf("Nb Services: %d\n", scalar @services);
+
+foreach my $serv (@services)
+{
+  my $ra   = Regexp::Assemble->new;
+  my @messages     = Octopussy::Service::Messages($serv);
+  foreach my $m (@messages)
+  {
+    # check if the regexp is valid
+    my $valid_regexp = eval {
+      use warnings FATAL => qw( regexp );
+      my $re = Octopussy::Message::Pattern_To_Regexp($m);
+      qr/$re/;
+    };
+
+    if ($valid_regexp)
+    {
+      my $regexp = Octopussy::Message::Pattern_To_Regexp($m);
+      $ra->add($regexp);
+    }
+  }
+  # Regexp::Assemble succeed to assemble ?
+  eval 
+  {
+    my $global = $ra->re; 
+  };
+  printf("Service %s => %s\n", $serv, ($@ ? "NOK => $@" : "OK"));
+  $nb_ok++  if (! $@);
+  $invalid{$serv} = 1 if ($@);
+}
+
+printf "Result: %s / %s\n", $nb_ok, scalar @services;
+foreach my $s (keys %invalid)
+{
+  print "$s\n";
+}
+
+1;
+
