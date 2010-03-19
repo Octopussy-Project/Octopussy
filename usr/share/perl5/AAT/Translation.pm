7a1d1cdffca605b717b10712de7da029420068ea
sebthebert <sebthebert@6a9e80cc-ef52-47e4-9be5-917275ac7a98>
2010-03-19 18:02:28 +0000

    AAT::Translation modified

--- usr/share/perl5/AAT/Translation.pm
+++ usr/share/perl5/AAT/Translation.pm
@@ -50,7 +50,6 @@ sub Get
 {
   my ($lang, $str) = @_;
 
-	$lang = lc($lang);
   return (undef) if (AAT::NULL($str));
   Init($lang) if (!defined $AAT_Translation{$lang}{'_USER'});
   $AAT_Translation{$lang}{$str} = (loc($str) || $str)
