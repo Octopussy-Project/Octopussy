7ddac1013b7c67d526ac8fae3a2634e611e8718b
sebthebert <sebthebert@6a9e80cc-ef52-47e4-9be5-917275ac7a98>
2010-05-07 17:22:18 +0000

    packaging_svn2debian.pl updated (using 'svn export' really better !)

--- packaging_svn2debian.pl
+++ packaging_svn2debian.pl
@@ -43,7 +43,7 @@ sub Octopussy_Version
 }
 
 
-=head2 Changelog
+=head2 Changelog()
 
 Generates /usr/share/doc/octopussy/changelog.gz file from changelog file
 
@@ -57,7 +57,7 @@ sub Changelog
 }
 
 
-=head2 Copy_Files
+=head2 Copy_Files()
 
 Copy files from SVN to temporary Debian packaging directory
 
@@ -68,42 +68,28 @@ sub Copy_Files
 	`cp ./copyright $DIR_TMP/usr/share/doc/$PACKAGE/`;
  
   printf "Copying Debian packaging files...\n";
-  mkpath("$DIR_TMP/DEBIAN/");
-  `cp -R ./DEBIAN/* $DIR_TMP/DEBIAN/`;
+  `svn export ./DEBIAN/ $DIR_TMP/DEBIAN/`;
   `chmod 755 $DIR_TMP/DEBIAN/`;
-  `chmod 555 $DIR_TMP/DEBIAN/{post,pre}{inst,rm}`;
+  `chmod 755 $DIR_TMP/DEBIAN/con*`;
+  `chmod 755 $DIR_TMP/DEBIAN/{post,pre}{inst,rm}`;
 
   printf "Copying Perl modules...\n";
-  mkpath("$DIR_TMP/usr/share/perl5/");
-  `cp -R ./usr/share/perl5/AAT* $DIR_TMP/usr/share/perl5/`;
-  `cp -R ./usr/share/perl5/Octopussy* $DIR_TMP/usr/share/perl5/`;
+  `svn export ./usr/share/perl5/ $DIR_TMP/usr/share/perl5/`;
   
   printf "Copying Configuration files...\n";
-  mkpath("$DIR_TMP/etc/aat/");
-  mkpath("$DIR_TMP/etc/$PACKAGE/");
-  mkpath("$DIR_TMP/var/lib/$PACKAGE/conf/");
-  `cp -R ./etc/aat/* $DIR_TMP/etc/aat/`;
-  `cp -R ./etc/$PACKAGE/* $DIR_TMP/etc/$PACKAGE/`;
-  `cp -R ./var/lib/$PACKAGE/conf/* $DIR_TMP/var/lib/$PACKAGE/conf/`;
-  `chmod -R 644 $DIR_TMP/etc/$PACKAGE/`;
-  mkpath("$DIR_TMP/etc/rsyslog.d/");
-	`cp ./etc/rsyslog.d/$PACKAGE.conf $DIR_TMP/etc/rsyslog.d/`;
-	`chmod -R 644 $DIR_TMP/etc/rsyslog.d/`; 
+  `svn export ./etc/ $DIR_TMP/etc/`;
+  mkpath("$DIR_TMP/var/lib/$PACKAGE/");
+  `svn export ./var/lib/$PACKAGE/conf/ $DIR_TMP/var/lib/$PACKAGE/conf/`;
+  `chmod -R 644 $DIR_TMP/etc/$PACKAGE/*`;
+	`chmod -R 644 $DIR_TMP/etc/rsyslog.d/*`; 
 
   printf "Copying Program files...\n";
-  mkpath("$DIR_TMP/usr/sbin/");
-  `cp ./usr/sbin/octo* $DIR_TMP/usr/sbin/`;
+  `svn export ./usr/sbin/ $DIR_TMP/usr/sbin/`;
   
   printf "Copying WebSite files...\n";
-  mkpath("$DIR_TMP/usr/share/aat/");
-  mkpath("$DIR_TMP/usr/share/$PACKAGE/");
-	`cp -R ./usr/share/aat/* $DIR_TMP/usr/share/aat/`;
-	`cp -R ./usr/share/$PACKAGE/* $DIR_TMP/usr/share/$PACKAGE/`;
-	
-	print "Removing svn files...\n";
-  `find $DIR_TMP -type d -name ".svn" -exec rm -rf \{\} \\;`;
+  `svn export ./usr/share/aat/ $DIR_TMP/usr/share/aat/`;
+  `svn export ./usr/share/$PACKAGE/ $DIR_TMP/usr/share/$PACKAGE/`;
   
-  rmtree("$DIR_TMP/etc/$PACKAGE/CA/");
   `rm -rf $DIR_TMP/var/lib/$PACKAGE/conf/{devicegroups,locations,schedule,servicegroups}.xml`;
   # Removing Catalyst stuff for now...
   rmtree('./usr/share/octopussy/Octopussy-Web/');
@@ -154,44 +140,27 @@ sub Man
 }
 
 
-=head2 MAIN
-
-=cut
+# ===== MAIN ===== #
 
 my $version = Octopussy_Version();
 my $filename_pkg = "${PACKAGE}_${version}_all.deb";
 
+# Removes old packaging stuff
 rmtree($DIR_TMP);
+unlink $filename_pkg;
+
 Man();
 Changelog();
 Copy_Files();
 
 `sed -i "s/^Version:.*/Version: $version/" ./$FILE_DEBIAN_CONTROL`;
-`dpkg-deb --build $DIR_TMP`;
-#`fakeroot dpkg-deb --build $DIR`;
-`mv ${DIR_TMP}.deb $filename_pkg`;
+`dpkg-deb --build $DIR_TMP $filename_pkg`;
 
-=head2 TODO
+rmtree($DIR_TMP);
 
-#
-# BUILDING PACKAGE
-#
-`chown -R root: $DIR/`; 
-#`chown -R root: $DIR/DEBIAN/*`;
-`find $DIR/usr/sbin/ -name "octo*" | xargs chmod 750`;
-`find $DIR/usr/share/perl5/Octopussy/Plugin/ -name "*" | xargs chmod 644`;
-`find $DIR/etc/aat/ -name "*.cnf" | xargs chmod 644`;
-`find $DIR -name "*.gz" | xargs chmod 644`;
-`find $DIR -name "*.asa" | xargs chmod 644`;
-`find $DIR -name "*.asp" | xargs chmod 644`;
-`find $DIR -name "*.html" | xargs chmod 644`;
-`find $DIR -name "*.js" | xargs chmod 644`;
-`find $DIR -name "*.pm" | xargs chmod 644`;
-`find $DIR -name "*.css" | xargs chmod 644`;
-`find $DIR -name "*.inc" | xargs chmod 644`;
-`find $DIR -name "*.gif" | xargs chmod 644`;
-`find $DIR -name "*.png" | xargs chmod 644`;
-`find $DIR -name "*.xml" -exec chmod 644 \{\} \\;`;
-`find $DIR -type d | xargs chmod 755`;
+
+=head1 AUTHOR
+
+Sebastien Thebert <octo.devel@gmail.com>
 
 =cut
