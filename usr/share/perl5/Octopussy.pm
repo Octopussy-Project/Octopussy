09b55d4b6e11a1bc920fd52a8b671aaafd67e386
sebthebert <sebthebert@6a9e80cc-ef52-47e4-9be5-917275ac7a98>
2013-05-23 22:36:50 +0000

    Octopussy 1.0.9 Release

--- usr/share/perl5/Octopussy.pm
+++ usr/share/perl5/Octopussy.pm
@@ -29,7 +29,7 @@ Readonly my $SF_SITE => 'http://sourceforge.net/projects/syslog-analyzer/files/'
 Readonly my $IDX_STAT_UID => 4;
 Readonly my $IDX_STAT_GID => 5;
 
-$Octopussy::VERSION = '1.0.8';
+$Octopussy::VERSION = '1.0.9';
 
 =head1 FUNCTIONS
 
