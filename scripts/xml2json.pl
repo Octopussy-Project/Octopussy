dc4646853755efe64a077d3058953d2fe46808b7
Sebastien Thebert <sebthebert@gmail.com>
2014-11-10 13:30:47 +0100

    Update xml2json.pl

--- scripts/xml2json.pl
+++ scripts/xml2json.pl
@@ -6,7 +6,7 @@ xml2json.pl - Script to migrate Octopussy XML configuration files to JSON
 
 =head1 SYNOPSIS
 
-xml2json.pl <filename.xml>
+xml2json.pl <filename.xml> [<filename2.xml> ... <filenameX.xml>]
 find /var/lib/octopussy/conf -iname *.xml | xml2json.pl
 
 =cut
@@ -22,26 +22,56 @@ use XML::Simple;
 my %action = (
 	octopussy_device => \&json_device,
 	octopussy_service => \&json_service,
+	octopussy_table => \&json_table,
 	);
+
+=head1 SUBROUTINES
+
+=head2 json_device($conf)
+
+=cut
 
 sub json_device
 {
 	printf "Device\n";
 }
 
+=head2 json_service($conf)
+
+=cut
+
 sub json_service
 {
 	my $conf = shift;
 
 	my @messages = ();
-    foreach my $m (sort { $a->{rank} cmp $b->{rank} } @{$conf->{message}})
-    {
-        delete $m->{rank};
-        push @messages, $m;
+	foreach my $m (sort { $a->{rank} cmp $b->{rank} } @{$conf->{message}})
+	{
+    	delete $m->{rank};
+    	push @messages, $m;
     }
     delete $conf->{message};
     delete $conf->{nb_messages};
     $conf->{messages} = \@messages;
+
+    return (to_json($conf, {pretty => 1}));
+}
+
+=head2 json_table($conf)
+
+=cut
+
+sub json_table
+{
+	my $conf = shift;
+	
+	my @fields = ();
+	foreach my $f (sort { $a->{title} cmp $b->{title} } @{$conf->{field}})
+    {
+    	push @fields, $f;
+    }
+    delete $conf->{field};
+    $conf->{fields} = \@fields;
 
     return (to_json($conf, {pretty => 1}));
 }
@@ -55,11 +85,11 @@ Read XML file '$filename'
 sub xml_read
 {
 	my $filename = shift;
+	
+	my %XML_INPUT_OPTIONS = (KeepRoot => 1, KeyAttr => [], ForceArray => 1);
 
-    my %XML_INPUT_OPTIONS = (KeepRoot => 1, KeyAttr => [], ForceArray => 1);
-
-    if ((defined $filename) && (-f $filename))
-    {
+	if ((defined $filename) && (-f $filename))
+	{
     	my $conf = eval { XMLin($filename, %XML_INPUT_OPTIONS); };
         die "[ERROR] Unable to read XML file $filename"	if ($EVAL_ERROR);
 
